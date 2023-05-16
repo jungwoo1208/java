fcf86973302ed72a14f46f87b8b38e8c8d6964ed
jungwoo <gjw1972@naver.com>
2023-05-16 14:07:59 +0900

    modified

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            System.out.println(2 * i);
+        }
     }
 }
