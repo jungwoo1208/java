5f5685e0a82575855e9117672f4fd607ca775bdf
jungwoo <gjw1972@naver.com>
2023-05-16 20:02:01 +0900

    da

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("AssertionError");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String m = sc.next();
+        int a = Integer.parseInt(m.substring(0, 1));
+        int b = Integer.parseInt(m.substring(1, 2));
+        int c = Integer.parseInt(m.substring(2, 3));
+        int d = Integer.parseInt(m);
+        System.out.println(n * c);
+        System.out.println(n * b);
+        System.out.println(n * a);
+        System.out.println(n * d);
+
     }
 }
